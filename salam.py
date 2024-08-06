701bd8af13489510c8fdaa76579f5a18be27eb65
aref-313-mahdi <aref4030@gmail.com>
2024-08-06 21:25:42 +0330

    تمام متن ها در یک فایل متنی

--- salam.py
+++ salam.py
@@ -1,1 +1,13 @@
-print('god bles you')
+import os
+os.chdir(r'The path of the folder where the text files are locatedمسیر پوشه ای که فایل های متن توش قرار داره')
+d=(os.listdir())
+
+for i in d:
+    with open(i,'a') as f:
+        f.write('-------------------------------------------------------------------------')
+
+with open('tamam moton', 'a', encoding="utf-8") as c:
+    for i in d:
+        with open (i,'r', encoding="utf-8") as s:
+            c.write(s.read())
+            c.write('\n')
